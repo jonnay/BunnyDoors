48b433079bfe7114945d67edfc182e2c26e63b49
Jonathan Arkell <jonnay@jonnay.net>
2012-01-29 18:37:18 -0700

    Added the info command to the help list.  now 0.1!

--- src/main/java/net/jonnay/bunnydoors/BunnyDoorsCommandExecutor.java
+++ src/main/java/net/jonnay/bunnydoors/BunnyDoorsCommandExecutor.java
@@ -5,7 +5,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
-
+import org.bukkit.block.Block;
 
 public class BunnyDoorsCommandExecutor implements CommandExecutor {
 	BunnyDoors plugin;
@@ -19,20 +19,22 @@ public class BunnyDoorsCommandExecutor implements CommandExecutor {
 		if (!cmd.getName().equalsIgnoreCase("bunnydoor")) {
 			return false;
 		}
-
+		
 		if (args.length < 1) {
 			usage(sender);
 			return false;
 		}
 
 		String subArg = args[0];
-
-	if (subArg.equalsIgnoreCase("lockall")) {
+		
+		if (subArg.equalsIgnoreCase("lockall")) {
 			lockall(sender);
 			return true;
 		} else if (subArg.equalsIgnoreCase("unlockall")) {
 			unlockall(sender);
 			return true;
+		} else if (subArg.equalsIgnoreCase("info")) {
+			return info(sender);
 		} else if (subArg.equalsIgnoreCase("help")) {
 			usage(sender);
 			return true;
@@ -43,8 +45,9 @@ public class BunnyDoorsCommandExecutor implements CommandExecutor {
 	}
 
 	private void usage(CommandSender s) {
-		s.sendMessage(usageLine("lockall", "Locks all doors"));
-		s.sendMessage(usageLine("unlockall","unlock all doors"));
+		s.sendMessage(usageLine("info", "Shows info about a door"));
+		s.sendMessage(usageLine("lockall", "Locks all the doors!"));
+		s.sendMessage(usageLine("unlockall","Unlock all the doors!"));
 		s.sendMessage(usageLine("help","(You're looking at it .. chum!"));
 	}
 
@@ -68,6 +71,29 @@ public class BunnyDoorsCommandExecutor implements CommandExecutor {
 		sender.sendMessage(ChatColor.WHITE+"Unlocking ALL THE DOORS!");
 
 		plugin.unlockAll(p);
+	}
+
+	private boolean info(CommandSender sender) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED+" You must be a player to do this!");
+			return false;
+		}
+
+		Player p = (Player) sender;
+		
+		Block b = p.getTargetBlock(null, 10);
+
+		String msg = "Door: "+
+			"Meta: "+Byte.toString(b.getData())+"  "+
+			"W:"+b.getWorld().getName()+"  "+
+			"x:"+b.getX()+" "+
+			"y:"+b.getY()+" "+
+			"z:"+b.getZ()+" ";
+		
+		
+		sender.sendMessage(msg);
+
+		return true;
 	}
 	
 }
